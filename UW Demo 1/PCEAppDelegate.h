f54addc0e28830a194b7ffc43536002511b53a79
Jordan Dobson <jordandobson@gmail.com>
2014-07-08 01:32:45 -0700

    following along on week one video

--- UW Demo 1/PCEAppDelegate.h
+++ UW Demo 1/PCEAppDelegate.h
@@ -8,8 +8,21 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface PCEAppDelegate : NSObject <NSApplicationDelegate>
+// Public Interface
+// All the things I want to expose
+// Model Object is typically a NSObject
+//
+
+@interface PCEAppDelegate : NSObject <NSApplicationDelegate, NSSpeechSynthesizerDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+
+// Make a Type of Thing to use on the interface or elsewhere publicly
+
+@property IBOutlet NSTextField          *theInputField;
+@property NSButton             *theRecordButton;
+
+@property          NSMutableArray       *theArray;
+@property          NSSpeechSynthesizer  *synthesizer;
 
 @end
--- UW Demo 1/PCEAppDelegate.m
+++ UW Demo 1/PCEAppDelegate.m
@@ -12,7 +12,72 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+
+    // Two stage initiation
+    // creating space in memory for array
+
+    NSArray  *voices  = [NSSpeechSynthesizer availableVoices];
+    NSString *myVoice = voices[ voices.count - 1 ];
+
+    // MAKE SOME OBJECTS
+    self.theArray    = [[NSMutableArray alloc] init];
+    self.synthesizer = [[NSSpeechSynthesizer alloc] initWithVoice:myVoice];
+
+    self.synthesizer.delegate = self;
+
+    //    NSLog(@"%@", voices);
+    //    NSLog(@"%@", @(voices.count));
+
+    //  PART 3
+    //  self.theInputField.textColor = [NSColor redColor];
+    //  self.theInputField.stringValue = @"Hello World.";
+    //  PART 2
+    //  NSString *myName = @"Jordan";
+    //  NSLog(@"%@", myName);
+    //  NSLog(@"%lu", [myName length]);
+    //  NSArray *myArray = @[ myName ];
+    //  PART 1
+    //  NSLog(@"Hello World");
+    //  NSLog(@"%i", 3);
+    //  NSLog(@"%10.2f", 3.14);
+    //  NSInteger myInteger = 456;
+
 }
+
+-(void)speechSynthesizer:(NSSpeechSynthesizer *)sender didFinishSpeaking:(BOOL)finishedSpeaking
+{
+    NSLog(@"Stopped Speaking");
+}
+
+-(IBAction)recordInput:(id)sender
+{
+    // Get current Input
+    NSString *curInput = _theInputField.stringValue;
+    // Put In Array
+    [self.theArray addObject:curInput];
+
+    NSLog(@"%@", self.theArray);
+    _theInputField.stringValue = @"";
+}
+
+- (IBAction)speakInput:(id)sender
+{
+    [self.synthesizer startSpeakingString: [_theArray componentsJoinedByString:@" "]];
+}
+
+//-(IBAction)helloButtonPushed:(id)sender
+//{
+//    // self.theInputField.stringValue = @"hello";
+//    [[self theInputField] setStringValue: @"Hello"];
+//}
+//
+//-(IBAction)goodbyeButtonPushed:(id)sender
+//{
+//    NSString *theString       = _theInputField.stringValue;
+//    NSString *theMethodString = [self.theInputField stringValue];
+//
+//    NSLog(@"%@", theString);
+//    NSLog(@"%@", theMethodString);
+//}
 
 @end
